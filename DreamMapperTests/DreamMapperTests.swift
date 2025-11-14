7beee9220d319c446c2cd491d614c91c9d7cffa1
clbonoan <clbonoan@gmail.com>
2025-11-13 23:46:16 -0800

    Initial Commit

---
+++ DreamMapperTests/DreamMapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamMapperTests.swift
+//  DreamMapperTests
+//
+//  Created by Christine Bonoan on 11/13/25.
+//
+
+import Testing
+@testable import DreamMapper
+
+struct DreamMapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
